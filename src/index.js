75271af2b3d113212aadf8e7efe653ad0d815d55
Achyut Pokhrel <achyutpokhrel@lftechnology.com>
2017-02-28 22:47:30 +0545

    first commit

---
+++ src/index.js
@@ -0,0 +1,7 @@
+require('../assets/css/style.css');
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import App from './components/App';
+ReactDOM.render(<App/>, document.querySelector('#app-container'));
---
+++ webpack.production.config.js
@@ -0,0 +1,29 @@
+/**
+ * Author: Pratish Shrestha <pratishshrestha@lftechnology.com>
+ * on 8/2/16.
+ */
+
+var webpack = require('webpack');
+
+//plugins
+var ExtractTextPlugin = require("extract-text-webpack-plugin");
+
+module.exports = {
+  entry: __dirname + "/src/index.js",
+  output: {
+    path: __dirname + '/dist',
+    publicPath: '/dist',
+    filename: 'bundle.js'
+  },
+  module: {
+    loaders: [
+      {test: /\.css$/, loader: ExtractTextPlugin.extract("style-loader", "css-loader")},
+      {test: /\.js$/, exclude: /node_modules/, loaders: ["react-hot", "babel-loader"]},
+      {test   : /\.(ttf|eot|svg|woff(2)?)(\?[a-z0-9=&.]+)?$/, loader : 'file-loader', query: {name: 'fonts/[name].[ext]'}},
+    ]
+  },
+  plugins: [
+    new ExtractTextPlugin("bundle.css"),
+    new webpack.optimize.UglifyJsPlugin()
+  ]
+};
