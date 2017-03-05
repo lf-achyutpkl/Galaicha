83f5701b3025af28a0ebfe6ca74b2a33b779b329
Achyut Pokhrel <achyutpokhrel@lftechnology.com>
2017-03-05 22:55:18 +0545

    khai

--- src/pages/CustomDesign.js
+++ src/pages/CustomDesign.js
@@ -72,25 +72,13 @@ class App extends Component {
 
     console.log(height, ' , ', width)
     if (height === '' || width === '') return;
-
-    console.log(height, ' , ', width)
-
     this.setState({width, height})
-
   }
 
   _saveImage = () => {
     html2canvas($("#widget"), {
       useCORS: true,
       onrendered: function (canvas) {
-        console.log(canvas);
-        // document.body.appendChild(canvas);
-
-        // Convert and download as image
-        // Canvas2Image.saveAsPNG(canvas);
-        // Clean up
-        //document.body.removeChild(canvas);
-
         var img = canvas.toDataURL()
         window.open(img);
       }
